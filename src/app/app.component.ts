4d442ddd5315a1a1f090ab4a5d102b7e78bfd314
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-05-18 00:31:02 +0100

    TpBankFrontend2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CustomersComponent } from './customers/customers.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  RouterModule,NavbarComponent,CustomersComponent,AccountsComponent,HttpClientModule],
+  imports: [RouterOutlet,  RouterModule,NavbarComponent,CustomersComponent,AccountsComponent,HttpClientModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,24 +1,35 @@
 import { Component, OnInit  } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {CustomerService } from '../services/customer.service'
+import {CustomerService } from '../services/customer.service';
+import { catchError } from 'rxjs/operators';
+import {  Observable,throwError } from 'rxjs';
+import {   ReactiveFormsModule,FormGroup ,FormBuilder } from '@angular/forms';
+
+import {Customer } from '../model/customer.model';
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
 export class CustomersComponent implements OnInit {
-  customers : any;
-constructor(private customerService: CustomerService){}
+  customers! : Observable<Array<Customer>>;
+  errorMessage!: string;
+  searchFormGroup :FormGroup | undefined;
+constructor(private customerService: CustomerService,private fb:FormBuilder){}
 ngOnInit():void{
-  this.customerService.getCustomers().subscribe(
-    {next : (data :any)=>{
-                      this.customers=data;
-                      },
-                    error :(err : any)=>{console.log(err);}
-                    });
+  this.searchFormGroup=this.fb.group({
+    keyword :this.fb.control("")});
+this.customers=this.customerService.getCustomers().pipe(
+  catchError(err =>{
+
+    this.errorMessage=err.message;
+    return  throwError(err);})
+
+  );
 
 
   }
+handleSearchCustomers(){}
 }
